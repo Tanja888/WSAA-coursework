6151f9cc3f5e17a159c9f5747f9986e331a7ee7c
Tanja Juric <G00411329@atu.ie>
2024-02-29 11:27:04 +0000

    Readme Current weather

---
+++ assignments/currentweather.py
@@ -0,0 +1,28 @@
+# Tanja Juric
+# Assignment: Current weather
+# Write a python program called currentweather.py that will print out:
+# the current temperature on the console (and only the temperature)
+# Also print the current wind direction (10m) from: https://open-meteo.com/
+
+import requests
+import json
+
+url1 = "https://api.open-meteo.com/v1/forecast?latitude=53.82&longitude=-9.5&current=temperature_2m,wind_speed_10m"
+response = requests.get(url1)
+# print(response)
+data = response.json()
+
+current = data["current"]
+temperature = current["temperature_2m"]
+print("Current temperature:", temperature)
+
+
+url2 = "https://api.open-meteo.com/v1/forecast?latitude=53.82&longitude=-9.5&current=temperature_2m,wind_direction_10m&forecast_days=1"
+response = requests.get(url2)
+# print(response)
+data = response.json()
+
+current = data["current"]
+# temperature = current["temperature_2m"]
+wind_direction = current["wind_direction_10m"]
+print("Wind direction 10m:", wind_direction )
